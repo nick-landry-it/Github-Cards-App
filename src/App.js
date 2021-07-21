15db79b9664b4ed3411187258ae04708f24c04f9
nick-landry-it <nicklandryit@gmail.com>
2021-07-21 17:01:11 -0500

    Reading the State Object

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import "./App.css"
+import React from "react"
 
 const testData = [
   {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
@@ -6,19 +7,18 @@ const testData = [
   {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
 ];
 
-const CardList = (props) => {
+const CardList = (props) => (
   <div>
-    <Card {...testData[0]}/>
-    <Card {...testData[1]}/>
+    {testData.map(profile => <Card {...profile} />)}
   </div>
-};
+);
 
 class Card extends React.Component {
   render() {
     const profile = this.props;
     return (
     <div className="github-profile">
-      <img src={profile.avatar_url}/>
+      <img src={profile.avatar_url} alt=""/>
       <div className="info">
         <div className="name">{profile.name}</div>
         <div className="company">{profile.company}</div>
@@ -28,11 +28,23 @@ class Card extends React.Component {
   }
 }
 
+class Form extends React.Component {
+  render() {
+    return (
+        <form action="">
+          <input type="text" placeholder="GitHub Username" />
+          <button>Add Card</button>
+        </form>
+    )
+  }
+}
+
 class App extends React.Component {
   render() {
     return (
       <div>
         <div className="header">{this.props.title}</div>
+        <Form />
         <CardList />
       </div>
     );
